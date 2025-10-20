334a5f94beb8daf12468c87f52dbf24124c90518
asterheng <siheng1997@gmail.com>
2025-10-20 10:20:21 +0800

    Update UserController.py

--- Team7/app/control/UserController.py
+++ Team7/app/control/UserController.py
@@ -10,8 +10,19 @@ class CreateUserController:
         
         
 class ListUserController:
-    def list_all_users(self):
-        return UserAccount.list_all()
+    def list_all_users(self, page=1, per_page=20):
+        return UserAccount.list_all(page=page, per_page=per_page)
 
-def get_user_by_id(self, user_id: int):
-        return UserAccount.get_by_id(user_id)
+    def get_user_by_id(self, user_id: int):
+            return UserAccount.get_by_id(user_id)
+
+class UserSearchController:
+    def search(self, term: str, page: int | None = None, per_page: int = 20):
+        return UserAccount.search(term, page=page, per_page=per_page)
+
+class UpdateUserController:
+    def update(self, user_id, name, email, password, profile_id, is_suspended):
+        return UserAccount.update_user(user_id, name, email, password, profile_id, is_suspended)
+
+    def get(self, user_id):
+        return UserAccount.query.get(user_id)
