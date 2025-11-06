dc88a2905cde0fa91954305a6ed2379d82f2ce97
asterheng <siheng1997@gmail.com>
2025-11-06 21:49:04 +0800

    Delete Team7/app/control/SessionController.py

--- Team7/app/control/SessionController.py
+++
@@ -1,8 +0,0 @@
-# app/control/session_controller.py
-from ..entity.SessionUser import SessionUser
-
-class SessionController:
-    def login(self, email: str, password: str):
-        """Controller: Pure data passing"""
-        user = SessionUser.authenticate_user(email, password)
-        return {"ok": bool(user), "user": user}
